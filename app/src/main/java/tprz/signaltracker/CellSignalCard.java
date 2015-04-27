91c07aa8b5de4ed398413a5ccf525c977f394c64
Thomas Przepiorka <tprzepiorka@gmail.com>
2015-04-27 21:45:28 +0100

    Basic graph dimensions and setup

--- app/src/main/java/tprz/signaltracker/CellSignalCard.java
+++ app/src/main/java/tprz/signaltracker/CellSignalCard.java
@@ -8,6 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardThumbnail;
 
@@ -19,6 +28,8 @@ import it.gmariotti.cardslib.library.internal.CardThumbnail;
 public class CellSignalCard extends Card {
     CellSignalListener cellSignalListener;
     private TextView cellSignalTextView;
+    private LineChart chart;
+    private int chartWidth = 30;
     private int[] cellSignalDrawables = new int[] {
             R.drawable.ic_signal_cellular_0_bar_grey600_48dp,
             R.drawable.ic_signal_cellular_1_bar_grey600_48dp,
@@ -63,12 +74,73 @@ public class CellSignalCard extends Card {
         thumbnail.setDrawableResource(cellSignalDrawables[iconLevel]);
         addCardThumbnail(thumbnail);
         notifyDataSetChanged();
+
+        // Charting
+        updateChart(signalStrength);
+    }
+
+    private void initChart() {
+        chart.setVisibleXRange(50);
+        chart.getAxis(YAxis.AxisDependency.LEFT).setAxisMinValue(0);
+        chart.getAxis(YAxis.AxisDependency.LEFT).setAxisMaxValue(32);
+        chart.getAxis(YAxis.AxisDependency.LEFT).setLabelCount(2);
+        chart.getAxis(YAxis.AxisDependency.RIGHT).setEnabled(false);
+        chart.setVisibleYRange(32, YAxis.AxisDependency.RIGHT);
+        chart.getXAxis().setDrawGridLines(false);
+
+        ArrayList<String> x = new ArrayList<>();
+        List<Entry> y = new ArrayList<>();
+        for(int i =0; i<30;i++) {
+            x.add("" + i);
+            y.add(new Entry(0, i));
+        }
+        LineDataSet dataSet = new LineDataSet(y, "ASU");
+        LineData lineData = new LineData(x, dataSet);
+        chart.setData(lineData);
+    }
+
+    /***
+     *
+     * http://stackoverflow.com/questions/28105547/how-to-make-a-realtime-rollingwindow-graph-using-mpandroidchart
+     * @param signalStrength
+     */
+    private void updateChart(int signalStrength) {
+        LineData lineData = chart.getData();
+        LineDataSet lineDataSet = lineData.getDataSetByIndex(0);
+        int count = lineDataSet.getEntryCount();
+
+
+        if (lineData.getXValCount() <= count) {
+            // Remove/Add XVal
+//            lineData.getXVals().add("" + count);
+//            lineData.getXVals().remove(0);
+
+            // Move all entries 1 to the left..
+            for (int i=0; i < count; i++) {
+                Entry e = lineDataSet.getEntryForXIndex(i);
+                if (e==null) continue;
+
+                e.setXIndex(e.getXIndex() - 1);
+            }
+
+            // Set correct index to add value
+            count = chartWidth;
+        }
+
+        // Add new value
+        lineData.addEntry(new Entry(signalStrength, count), 0);
+
+        // Make sure to draw
+        chart.notifyDataSetChanged();
+        chart.invalidate();
     }
 
     @Override
     public void setupInnerViewElements(ViewGroup parent, View view) {
         super.setupInnerViewElements(parent, view);
         cellSignalTextView = (TextView) parent.findViewById(R.id.cell_signal_text);
+        this.chart = (LineChart) parent.findViewById(R.id.chart);
+        initChart();
     }
 
     public class CellSignalListener extends PhoneStateListener {
